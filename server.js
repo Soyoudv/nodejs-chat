f8b03f054fb34139f13c6ee53b769bc6023e1dfe
Soyoudv <noname54765@gmail.com>
2025-11-22 15:47:46 +0100

    client.html and server.js change

--- server.js
+++ server.js
@@ -11,6 +11,46 @@ app.get('/', (req, res) => { // Envoie au client le fichier client.html
     res.sendFile(__dirname + '/client.html');
 });
 
+let user_list = []; // liste des utilisateurs connectés
+let user_needed = 2; // nombre d'utilisateurs nécessaires pour démarrer la partie
+let user_max = 4; // nombre maximum d'utilisateurs
+let game_going = false; // indique si une partie est en cours
+
+function update_all_user_list() {
+  console.log("envoi de la nouvelle liste des utilisateurs à tous les clients");
+  io.emit('update_user_list', user_list, user_needed, user_max);
+}
+
+io.on('connection', (socket) => {
+
+  console.log('Un utilisateur est connecté');
+  socket.emit('update_user_list', user_list, user_needed, user_max);
+
+  socket.on('identification', (new_user) => {
+
+    console.log("essai de connexion avec le nom " + new_user);
+    
+    if (user_list.includes(new_user)) {
+
+      console.log("mais le nom est déjà pris");
+      socket.emit('join_response', new_user, false, 'Nom déjà pris');
+
+    }else{
+
+      user_list.push(new_user);
+
+      console.log("nom ajouté à la liste");
+      socket.emit('join_response', new_user, true, 'Nom accepté');
+
+      update_all_user_list();
+
+    }
+
+  });
+  
+});
+
 server.listen(8888, () => { // Démarrage du serveur sur le port 8888
   console.log('Serveur lancé sur http://localhost:8888');
 });
+
