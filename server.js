e925560faf7fb9d32120503ed8dc303dc23fa9de
Soyoudv <noname54765@gmail.com>
2025-11-22 14:52:19 +0100

    void boites on startup

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
-server.listen(8888, () => {
+const { on, once } = require('events'); // Importation des fonctions on et once du module events
+const express = require('express'); // Importation du framework Express
+const http = require('http'); // Importation du module HTTP
+const socketIo = require('socket.io'); // Importation de Socket.IO
+
+const app = express(); // Création de l'application Express
+const server = http.createServer(app); // Création du serveur HTTP
+const io = socketIo(server); // Initialisation de Socket.IO avec le serveur HTTP
+
+app.get('/', (req, res) => { // Envoie au client le fichier client.html
+    res.sendFile(__dirname + '/client.html');
+});
+
+server.listen(8888, () => { // Démarrage du serveur sur le port 8888
   console.log('Serveur lancé sur http://localhost:8888');
 });
